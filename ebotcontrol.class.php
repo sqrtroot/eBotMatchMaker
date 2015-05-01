316dbb3ec796e38f899fca6fe875efbc5b775332
Uniquoooo <cheese5505@gmail.com>
2015-05-01 19:04:21 +1200

    Inserted matches with teamid instead of teamname

--- ebotcontrol.class.php
+++ ebotcontrol.class.php
@@ -40,7 +40,7 @@ class eBotController {
 	}
 
 	public function getAssoc($query) {
-		$assoc = mysql_fetch_assoc($query);
+		$assoc = mysql_fetch_assoc($query) or die(mysql_error());
 		return $assoc;
 	}
 
@@ -161,11 +161,13 @@ class eBotController {
 					$query_team2 = $this->query("SELECT * FROM `teams` WHERE `name`='$team2_name'");
 					$assoc_team1 = $this->getAssoc($query_team1);
 					$assoc_team2 = $this->getAssoc($query_team2);
+					$team1_ebotid = $assoc_team1['id'];
+					$team2_ebotid = $assoc_team2['id'];
 
 					$randomString = $this->randomString();
-					$creatematch = "INSERT INTO `matchs` (`id`, `ip`, `server_id`, `season_id`, `team_a`, `team_a_flag`, `team_a_name`, `team_b`, `team_b_flag`, `team_b_name`, `status`, `is_paused`, `score_a`, `score_b`, `max_round`, `rules`, `overtime_startmoney`, `overtime_max_round`, `config_full_score`, `config_ot`, `config_streamer`, `config_knife_round`, `config_switch_auto`, `config_auto_change_password`, `config_password`, `config_heatmap`, `config_authkey`, `enable`, `map_selection_mode`, `ingame_enable`, `current_map`, `force_zoom_match`, `identifier_id`, `startdate`, `auto_start`, `auto_start_time`, `created_at`, `updated_at`) VALUES (NULL, NULL, NULL, " . $this->eBotTeamSettings['seasonid'] . ", NULL, '" . $this->eBotTeamSettings['teamflag'] . "', '$team1_name', NULL, '" . $this->eBotTeamSettings['teamflag'] . "', '$team2_name', '0', NULL, '0', '0', '" . $this->eBotMatchSettings['maxround'] . "', '" . $this->eBotMatchSettings['rules'] . "', '" . $this->eBotMatchSettings['overtime_startmoney'] . "', '" . $this->eBotMatchSettings['overtime_mr'] . "', '0', '" . $this->eBotMatchSettings['overtime'] . "', '" . $this->eBotMatchSettings['streamer'] . "', '" . $this->eBotMatchSettings['knife'] . "', NULL, NULL, '" . $randomString . "', NULL, NULL, NULL, 'normal', NULL, NULL, NULL, NULL, CURRENT_DATE(), '0', '5', CURRENT_DATE(), CURRENT_DATE())";
+					$creatematch = "INSERT INTO `matchs` (`id`, `ip`, `server_id`, `season_id`, `team_a`, `team_a_flag`, `team_a_name`, `team_b`, `team_b_flag`, `team_b_name`, `status`, `is_paused`, `score_a`, `score_b`, `max_round`, `rules`, `overtime_startmoney`, `overtime_max_round`, `config_full_score`, `config_ot`, `config_streamer`, `config_knife_round`, `config_switch_auto`, `config_auto_change_password`, `config_password`, `config_heatmap`, `config_authkey`, `enable`, `map_selection_mode`, `ingame_enable`, `current_map`, `force_zoom_match`, `identifier_id`, `startdate`, `auto_start`, `auto_start_time`, `created_at`, `updated_at`) VALUES (NULL, NULL, NULL, " . $this->eBotTeamSettings['seasonid'] . ", '$team1_ebotid', NULL, NULL, '$team2_ebotid', NULL, NULL, '0', NULL, '0', '0', '" . $this->eBotMatchSettings['maxround'] . "', '" . $this->eBotMatchSettings['rules'] . "', '" . $this->eBotMatchSettings['overtime_startmoney'] . "', '" . $this->eBotMatchSettings['overtime_mr'] . "', '0', '" . $this->eBotMatchSettings['overtime'] . "', '" . $this->eBotMatchSettings['streamer'] . "', '" . $this->eBotMatchSettings['knife'] . "', NULL, NULL, '" . $randomString . "', NULL, NULL, NULL, 'normal', NULL, NULL, NULL, NULL, CURRENT_DATE(), '0', '5', CURRENT_DATE(), CURRENT_DATE())";
 					$query_creatematch = $this->query($creatematch);
-					$query_matchid = $this->query("SELECT * FROM `matchs` WHERE `team_a_name`='$team1_name' AND `team_b_name`='$team2_name'");
+					$query_matchid = $this->query("SELECT * FROM `matchs` WHERE `team_a`='$team1_ebotid' AND `team_b`='$team2_ebotid'");
 					$assoc_matchid = $this->getAssoc($query_matchid);
 					$matchid2 = $assoc_matchid['id'];
 					$query_maps = $this->query("INSERT INTO `maps` (`id`, `match_id`, `map_name`, `score_1`, `score_2`, `current_side`, `status`, `maps_for`, `nb_ot`, `identifier_id`, `tv_record_file`, `created_at`, `updated_at`) VALUES ('" . $matchid2 . "', '" . $matchid2 . "', 'tba', '0', '0', 'ct', '0', 'default', '0', NULL, NULL, CURRENT_DATE(), CURRENT_DATE())");
